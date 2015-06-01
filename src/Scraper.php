bf8d5d7e92828671f56141fee757babd76f0ce9b
Martin Lindhe <martin@ubique.se>
2015-06-02 00:05:30 +0200

    use UserAgentTrait

--- src/Scraper.php
+++ src/Scraper.php
@@ -2,10 +2,12 @@
 
 use Carbon\Carbon;
 use MartinLindhe\Traits\DiskCacheTrait;
+use MartinLindhe\UserAgentTrait\UserAgentTrait;
 
 class Scraper
 {
     use DiskCacheTrait;
+    use UserAgentTrait;
 
     /**
      * @param Carbon $date
@@ -43,6 +45,7 @@ class Scraper
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch,CURLOPT_USERAGENT, $this->userAgent);
 
         $output = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
